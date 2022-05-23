3ca8636e1bf4e8d79ad40661919381ec61fc41b1
Pranav-Talmale <pranavtalmale@gmail.com>
2022-05-23 08:46:54 +0530

    changes so that able to build with terminal access

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,16 +24,9 @@ if [ "$FOX_BRANCH" = "fox_11.0" ]; then
     touch frameworks/base/core/xsd/vts/Android.mk 2>/dev/null || echo
 fi
 
-# Prepare the Build Environment
-source build/envsetup.sh
 
 # Run the Extra Command
 $EXTRA_CMD
-
-# export some Basic Vars
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export LC_ALL="C"
 
 # Default Build Type
 export FOX_BUILD_TYPE="Unofficial"
@@ -48,28 +41,6 @@ BRANCH_INT=$(echo $SYNC_BRANCH | cut -d. -f1)
 if [[ $OF_USE_LATEST_MAGISK = "true" || $OF_USE_LATEST_MAGISK = "1" ]]; then
 	echo "Using the Latest Release of Magisk..."
 	export FOX_USE_SPECIFIC_MAGISK_ZIP=$("ls" ~/Magisk/Magisk*.zip)
-fi
-
-# Legacy Build Systems
-if [ $BRANCH_INT -le 6 ]; then
-    export OF_DISABLE_KEYMASTER2=1 # Disable Keymaster2
-    export OF_LEGACY_SHAR512=1 # Fix Compilation on Legacy Build Systems
-fi
-
-# lunch the target
-if [ "$BRANCH_INT" -ge 11 ]; then
-    lunch twrp_${DEVICE}-eng || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-else
-    lunch omni_${DEVICE}-eng || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-fi
-
-# Build the Code
-if [ -z "$J_VAL" ]; then
-    mka -j$(nproc --all) $TARGET || { echo "ERROR: Failed to Build OrangeFox!" && exit 1; }
-elif [ "$J_VAL"="0" ]; then
-    mka $TARGET || { echo "ERROR: Failed to Build OrangeFox!" && exit 1; }
-else
-    mka -j${J_VAL} $TARGET || { echo "ERROR: Failed to Build OrangeFox!" && exit 1; }
 fi
 
 # Exit
