6a1a9fb3b834e505d569bc07e9542a4a45f4b7f8
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-05-23 10:09:57 +0530

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -48,7 +48,7 @@ git clone $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!"
 [ ! -z "$KERNEL_SOURCE" ] && git clone --depth=1 --single-branch $KERNEL_SOURCE $KERNEL_PATH
 
 #Clone Neutron Clang
-git clone https://gitlab.com/dakkshesh07/neutron-clang.git ~/work/prebuilts/clang/host/linux-x86 clang-neutron 
+git clone https://gitlab.com/dakkshesh07/neutron-clang.git prebuilts/clang/host/linux-x86 clang-neutron 
 
 # Magisk
 if [[ $OF_USE_LATEST_MAGISK = "true" || $OF_USE_LATEST_MAGISK = "1" ]]; then
