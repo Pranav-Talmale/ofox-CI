c048517278e9dea1739ac949e7a3f48ea301f743
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-05-27 11:15:29 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_lisa.git -b inline-build"
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_lisa.git -b script-miui"
 
 export DEVICE="lisa"
 export OEM="xiaomi"
@@ -12,7 +12,7 @@ export OUTPUT="OrangeFox*.zip"
 
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-export KERNEL_SOURCE="https://github.com/Pranav-Talmale/android_kernel_xiaomi_lisa.git"
+#export KERNEL_SOURCE="https://github.com/Pranav-Talmale/android_kernel_xiaomi_lisa.git"
 #export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
@@ -27,8 +27,8 @@ OF_USE_LATEST_MAGISK=true
 # Custom Clang is needed if you face issues while building with a kernel source. Make sure to add appropriate flags in your BoardConfig.mk to use custom clang.
 # Can't clone both clangs. Uncomment the clang that you don't need.
 #OF_USE_NEUTRON_CLANG=true
-OF_USE_PROTON_CLANG=true
-export CUSTOM_CLANG_FOLDER="clang_13.0.0"
+#OF_USE_PROTON_CLANG=true
+#export CUSTOM_CLANG_FOLDER="clang_13.0.0"
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
