14f0886a9f3934c4a61f7716d6da9d6cde29da63
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-06-15 18:37:44 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-ofox.git -b kernel-build"
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-ofox.git -b test"
 
 export DEVICE="raphael"
 export OEM="xiaomi"
