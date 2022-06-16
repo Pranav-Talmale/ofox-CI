c8a0e21a9692bbc60a7af52dd98de690cc1d81d7
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-06-16 12:15:49 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-ofox.git -b kernel-build"
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-ofox.git -b test"
 
 export DEVICE="raphael"
 export OEM="xiaomi"
@@ -12,11 +12,11 @@ export OUTPUT="OrangeFox*.zip"
 
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-export KERNEL_SOURCE="https://github.com/Pranav-Talmale/kernel_xiaomi_raphael.git -b snaplight"
+#export KERNEL_SOURCE="https://github.com/Pranav-Talmale/kernel_xiaomi_raphael.git -b snaplight"
 #export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
-export EXTRA_CMD="export PATH="$(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin:$PATH""
+#export EXTRA_CMD="export PATH="$(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin:$PATH""
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
@@ -27,7 +27,7 @@ OF_USE_LATEST_MAGISK=true
 # Custom Clang is needed if you face issues while building with a kernel source. Make sure to add appropriate flags in your BoardConfig.mk to use custom clang.
 # Can't clone both clangs. Uncomment the clang that you don't need.
 #OF_USE_NEUTRON_CLANG=true
-OF_USE_PROTON_CLANG=true
+#OF_USE_PROTON_CLANG=true
 #export CUSTOM_CLANG_FOLDER="clang-proton"
 
 # Not Recommended to Change
