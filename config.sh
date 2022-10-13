2fb56b41e70757a6a6d92e8f11ea8246694d6841
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-10-13 16:44:14 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_raphael-twrp -b twrp-12.1"
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_lisa -b fox_12.1"
 
-export DEVICE="raphael"
+export DEVICE="lisa"
 export OEM="xiaomi"
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 # supported targets : 
 # 1. recoveryimage
@@ -21,7 +21,7 @@ export OUTPUT="OrangeFox*.zip"
 #export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
-export EXTRA_CMD="export FOX_VARIANT="FBEv2""
+#export EXTRA_CMD="export FOX_VARIANT="FBEv2""
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
@@ -35,7 +35,7 @@ export OF_USE_LATEST_MAGISK=true
 #OF_USE_PROTON_CLANG=true
 #export CUSTOM_CLANG_FOLDER="clang-proton"
 
-#export OF_CLONE_SKKK_PATCHES=true
+export OF_CLONE_SKKK_PATCHES=true
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
